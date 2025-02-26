86c0b26f7abf84f3ca13ed21d8f1dae727544fce
chaewon111 <gkcodnjsdlqj@naver.com>
2025-02-26 09:38:18 -0500

    F

--- Unit 2/HW/2-1HW/Mazeserching.py
+++ Unit 2/HW/2-1HW/Mazeserching.py
@@ -1,5 +1,5 @@
 
-import random
+import random, time,os
 file=open('hwmaze.txt','r')
 maze=[]
 while True:
@@ -7,23 +7,55 @@ while True:
     if line == "":
         break
     else:
-        maze.append(line.rstrip('\n').split())
+        maze.append(line.rstrip('\n').split(','))
 
 step=0
 
+def displayMaze():
+
+    #Slow down to watch simulation
+    time.sleep(0.2)
+    os.system("cls")
+
+    #Adjust output to make it look better on the screen
+    map = "\n\n"
+    for row in range(0,len(maze)):
+        for col in range(0,len(maze[row])):
+            if maze[row][col] == "1":
+                map = map + "[X]"
+
+            if maze[row][col] == "0":
+                map = map +  "   "
+
+            if maze[row][col] == ".":
+                map = map + " . "
+
+            if maze[row][col] == "2":
+                map = map + "   "
+
+            if maze[row][col] == "E":
+                map = map + " E "
+
+            if maze[row][col] == "S":
+                map = map + " S "
+
+        map = map + "\n"
+
+    print(map)
 
 def move(row,col):
    
-    global maze
+    global maze,step
 
     
     possibles = [[row,col+1],[row,col-1],[row-1,col],[row+1,col]]
     random.shuffle(possibles)
 
 
-    if maze[row][col] == "0" or maze[row][col] == "S":
+    if 0<=row <len(maze) and 0<=col <len(maze[0]) and (maze[row][col] == "0" or maze[row][col] == "s"):
         maze[row][col] = "."
-        step=step+1 # 가능한 인덱스 어팬드드
+        step=step+1 
+      
         
 
         if (move(possibles[0][0],possibles[0][1])):  
@@ -40,10 +72,11 @@ def move(row,col):
 
         else:
             maze[row][col] = "2"
+            
             return False
 
-    #Found exit
-    elif maze[row][col] == "E":
+    
+    elif  0<=row <len(maze) and 0<=col <len(maze[0])and ( maze[row][col] == "e"):
         maze[row][col] = "."
         return True
 
@@ -55,11 +88,13 @@ def main():
     for i in range(10):
         
         move(0,3)
-        
+       
         with open('hwmane2.txt', 'a') as file1:
             for row in maze:   
                 file1.write(' '.join([str(item) for item in row]))   
-                file1.write('\n')   
+                file1.write('\n')
+            file1.write('\n')
+               
         steps.append(step)
 
     maxx=max(steps)
@@ -68,4 +103,7 @@ def main():
     print(f'''max steps= {maxx}
             min dteps={minn}
             everage steps={ave}''')
+    
+
+main()
 
